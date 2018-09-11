58d0ef1a9774737579ef221aaefb4eec7c396d79
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-11 16:09:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_supplies.values.select {|el| el.values.include?("BBQ")}.keys
+  holiday_supplies.values.flatten
 end
 
 
